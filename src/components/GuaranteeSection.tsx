9a2059031fbd516024957303a34540d66b55d07b
juliocosta7 <jc274779@gmail.com>
2026-01-21 00:09:50 +0100

    Clean up GuaranteeSection component
    Fixed formatting issues and removed extraneous URLs.

--- src/components/GuaranteeSection.tsx
+++ src/components/GuaranteeSection.tsx
@@ -10,7 +10,7 @@ const paymentMethods = {
   mozambique: [
     { name: "M-Pesa", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/M-PESA_LOGO-01.svg/2560px-M-PESA_LOGO-01.svg.png" },
     { name: "E-Mola", image: "https://apps.apple.com/br/app/e-mola/id1484826743" }
-  ]https://play-lh.googleusercontent.com/wqwKGhfMxCvX1Aw9g8sLJNv2L8f6x-NyQqQfEE8FFnJKZrZvTZvvFnCBYGEpVKhMKQ=w240-h480-rw
+  ]
 };
 
 const GuaranteeSection = () => {
